581072259604e2d2c8c04243715713d8c853e418
rabsztok <rabsztok@gmail.com>
2018-07-19 07:58:20 +0200

    Use styled-components in preview iframe

--- src/cms/preview-templates/HomepagePreview.js
+++ src/cms/preview-templates/HomepagePreview.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { HomepageTemplate } from 'templates/homepage'
+import { StyleSheetManager } from 'styled-components'
 
-const HomepagePreview = ({ entry, widgetFor }) => (
-  <HomepageTemplate
-    logo={entry.getIn(['data', 'logo'])}
-    joinUsBackground={entry.getIn(['data', 'joinUsBackground'])}
-    joinUsTitle={entry.getIn(['data', 'joinUsTitle'])}
-    joinUsButtonText={entry.getIn(['data', 'joinUsButtonText'])}
-    joinUsButtonURL={entry.getIn(['data', 'joinUsButtonURL'])}
-  />
-)
+const HomepagePreview = ({ entry, widgetFor }) => {
+  const iframe = document.querySelector('.nc-previewPane-frame')
+  const iframeHeadElem = iframe.contentDocument.head
+
+  return (
+    <StyleSheetManager target={iframeHeadElem}>
+      <HomepageTemplate
+        logo={entry.getIn(['data', 'logo'])}
+        joinUsBackground={entry.getIn(['data', 'joinUsBackground'])}
+        joinUsTitle={entry.getIn(['data', 'joinUsTitle'])}
+        joinUsButtonText={entry.getIn(['data', 'joinUsButtonText'])}
+        joinUsButtonURL={entry.getIn(['data', 'joinUsButtonURL'])}
+      />
+    </StyleSheetManager>
+  )
+}
 
 HomepagePreview.propTypes = {
   entry: PropTypes.shape({
